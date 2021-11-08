294babcc96b6b36296ac8e112f5355c91b5a1a04
isg99 <isaacg99@gmail.com>
2021-11-07 20:54:18 -0600

    converted bmp to list of negative values

--- main.exs
+++ main.exs
@@ -1,7 +1,30 @@
 defmodule Image do
+
   def read(path) do
-    header=File.stream!(path,[],54)
-    # data = read 55 -> n bytes
-    IO.inspect(data)
+    file = File.open!(path, [:read, :binary])
+    bm = IO.binread(file, :all)
+  end
+
+  def makeNeg(pixels) do
+    newPixels = for tuple <- pixels do
+                  newList = Tuple.to_list(tuple)
+                  for val <- newList do
+                    if val > 0 do
+                      val - 255
+                    else
+                      val + 255
+                    end 
+                  end
+                end
+  end
+
+  def main() do
+    path = "img_2.bmp"
+    file = read(path)
+
+    header = String.slice(file,0..54)
+
+    pixels = for <<r::8, g::8, b::8 <- String.slice(file,54..String.length(file)-1)>>, do: {r, g, b}
+    makeNeg(pixels)
   end
 end
