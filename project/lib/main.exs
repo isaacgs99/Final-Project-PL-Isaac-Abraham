7e06f09a16f92e252ff4da2fe315a4cf0fc44950
AbrahamGarcia22 <A01023256@itesm.mx>
2021-12-01 22:21:07 -0600

    New comments

--- project/lib/main.exs
+++ project/lib/main.exs
@@ -1,27 +1,50 @@
-# Final Project: Instagram Filter simulator
+# Final Project: Image processing: Filters and rotation
 # Isaac Garza and Abraham García del Corral
-# 10-nov-2021
+# 2021-12-03
 
 defmodule Image do
+  @moduledoc """
+  This module contains functions in order to read, manipulate and write 
+  data from images.
+  """
 
+
+  @doc """
+  Read function receives the path of a file. The file must be a BMP image
+  in order to work as spected.
+  Once readed returns the binary information of the file.
+  """
   def read(path) do
     file = File.open!(path, [:read, :binary])
     _bm = IO.binread(file, :all)
   end
 
+
+  @doc """
+  Gets the list with the image data as argument as well as the name of the
+  desired output file. The last one has to incude the bmp extension in order
+  to display the image in the correct way
+  """
   def write(list, name) do
     file= File.open!(name, [:write])
     IO.binwrite(file, list)
     File.close(file)
   end
 
-  def makeNeg(pixels) do
+
+  
+  # Gets a bitsring as argument and for each value changes or not according to
+  # the formula
+  defp makeNeg(pixels) do
     _newPixels = for <<i <- pixels >> do
         255 - i
     end
   end
 
-  def check255?(val) do
+
+  # Aux function that checks if the value received is greater than 255 or not
+  # This function is used by makeSep function
+  defp check255?(val) do
       if val > 255 do 
           255
       else
@@ -30,7 +53,9 @@ defmodule Image do
   end
 
 
-  def check0?(val) do
+  # Aux function that checks if the value received is zero or not.
+  # Used by the mask function
+  defp check0?(val) do
       if val == 0 do 
           val
       else
@@ -38,6 +63,12 @@ defmodule Image do
       end
   end
 
+
+  @doc """
+  Receives tuple of R G B values as argument, and then modifies their values
+  according to the Sepia formula
+  Returns a List with new values
+  """
   def makeSep(pixels) do
     newPixels = for {r,g,b} <- pixels do
                   resR = trunc((0.393 * r) + (0.769 * g) + (0.189 * b))
@@ -49,6 +80,11 @@ defmodule Image do
     List.flatten(newPixels)
   end
 
+
+  @doc """
+  Gets a tuple of R G B as argument, and modifies their values according to
+  the formula. Returns a List with new values
+  """
   def makeBW(pixels) do
     newPixels = for {r,g,b} <- pixels do
                   res = ((0.3 * r) + ( 0.6 * g) + ( 0.1 * b));
@@ -59,6 +95,12 @@ defmodule Image do
     List.flatten(newPixels)
   end
 
+
+  @doc """
+  Takes a binary as argument, reverse the order of the data, and changes the
+  order of the corresponding RGB values.
+  Returns a List with the new data.
+  """
   def mirror(pixels) do
     revPixels = Enum.reverse(pixels)
     newPixels = for {r,g,b} <- revPixels do
@@ -67,11 +109,17 @@ defmodule Image do
     List.flatten(newPixels)
   end
 
+
+  @doc """
+  Receives binary data as argument, and for each value, changes the value or
+  not according to the check function.
+  """
   def mask(pixels) do
     _newPixels = for <<i <- pixels >> do
         check0?(i)
     end
   end
+
 
   def main() do
     path = "../../img.bmp"
